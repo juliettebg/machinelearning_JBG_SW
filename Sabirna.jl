3a6555a295b464fa1bd14ad8fb9b72b2c7e7e4a8
sabrinawaa <119061814+sabrinawaa@users.noreply.github.com>
2022-12-06 21:20:05 +0100

    sorted data-not sure last twolines work

--- Sabirna.jl
+++ Sabirna.jl
@@ -1,3 +1,21 @@
 using CSV
 using DataFrames
-df = DataFrame(CSV.File("/Users/sabo4ever/Sabrina/EPFL/machine learning/Project/test.csv"))
+using Pkg
+##
+using Distributions, Plots, MLJ, MLJLinearModels, Random, OpenML
+##
+Pkg.activate(temp = true)
+Pkg.develop(url = "https://github.com/jbrea/MLCourse")
+Pkg.activate(joinpath(Pkg.devdir(), "MLCourse"))
+
+##
+df = DataFrame(CSV.File("/Users/sabo4ever/Sabrina/EPFL/machine learning/Project/train.csv"))
+
+##
+dropmissing(df)
+predictors=select(df,Not(:labels))
+idx=std.(eachcol(predictors)) .!= 0
+df_clean_const = predictors[:, idx]                  #drop zero std predictors
+
+findall(≈(1), cor(Matrix(df_clean_const))) |> # find all indices with correlation ≈ 1
+idxs -> filter(x -> x[1] > x[2], idxs)
