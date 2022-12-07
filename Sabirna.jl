477f14ff4cb94aa70ec29e885c1faf1d18e2c3f5
sabrinawaa <119061814+sabrinawaa@users.noreply.github.com>
2022-12-07 16:09:59 +0100

    updated:last line doesnt work yet

--- Sabirna.jl
+++ Sabirna.jl
@@ -17,5 +17,9 @@ predictors=select(df,Not(:labels))
 idx=std.(eachcol(predictors)) .!= 0
 df_clean_const = predictors[:, idx]                  #drop zero std predictors
 
-findall(≈(1), cor(Matrix(df_clean_const))) |> # find all indices with correlation ≈ 1
+indices=findall(≈(1), cor(Matrix(df_clean_const))) |> # find all indices with correlation ≈ 1
 idxs -> filter(x -> x[1] > x[2], idxs)
+indices=DataFrame(indices)
+indices_ = union([indices[!,1][i][1] for i in 1:length(indices[!,1])])
+col_no=df_clean_const[:,indices_] #not right yet, here are all repeated ones.
+df_clean = df_clean_const[:, eachcol(df_clean_const).!= eachcol(col_no)]
